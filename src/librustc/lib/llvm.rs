aab6ad583d84cfde7ddb9ff83d30f816211d8542
Michael Woerister <michaelwoerister@gmail>
2013-07-02 18:10:24 +0200

    debuginfo: Support for tuple-style enums (WIP)

--- src/librustc/lib/llvm.rs
+++ src/librustc/lib/llvm.rs
@@ -2114,7 +2114,21 @@ pub mod llvm {
             AlignInBits: c_ulonglong,
             Elements: ValueRef,
             ClassType: ValueRef) -> ValueRef;
-}}
+
+        #[fast_ffi]
+        pub unsafe fn LLVMDIBuilderCreateUnionType(
+            Builder: DIBuilderRef,
+            Scope: ValueRef,
+            Name: *c_char,
+            File: ValueRef,
+            LineNumber: c_uint,
+            SizeInBits: c_ulonglong,
+            AlignInBits: c_ulonglong,
+            Flags: c_uint ,
+            Elements: ValueRef,
+            RunTimeLang : c_uint) -> ValueRef;
+    }
+}
 
 pub fn SetInstructionCallConv(Instr: ValueRef, CC: CallConv) {
     unsafe {
