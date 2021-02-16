e367bab1b680b6c841164a67721dff5b02a02459
Michael Woerister <michaelwoerister@posteo>
2021-02-16 14:02:59 +0100

    Temporarily allow odht crate from git for testing.

--- src/tools/tidy/src/extdeps.rs
+++ src/tools/tidy/src/extdeps.rs
@@ -4,7 +4,10 @@ use std::fs;
 use std::path::Path;
 
 /// List of allowed sources for packages.
-const ALLOWED_SOURCES: &[&str] = &["\"registry+https://github.com/rust-lang/crates.io-index\""];
+const ALLOWED_SOURCES: &[&str] = &[
+    "\"registry+https://github.com/rust-lang/crates.io-index\"",
+    "\"git+https://github.com/michaelwoerister/odht?branch=incoming#150c1553a123a4c54b59969a54895b7e0956975c\"",
+];
 
 /// Checks for external package sources. `root` is the path to the directory that contains the
 /// workspace `Cargo.toml`.
